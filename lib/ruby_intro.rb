847f3db135de89c9caf1ee8633952cc8851d7516
EC2 Default User <ec2-user@ip-172-31-41-253.us-east-2.compute.internal>
2020-01-30 19:59:08 +0000

    first commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,61 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).inject(0,:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+   return false if arr.empty?
+   m = Hash.new
+   arr.each do |i|
+       if m.has_key?(i)
+          return true
+       else
+          m[n-i] = i
+       end
+   end
+   return false
 end
 
 # Part 2
-
 def hello(name)
-  # YOUR CODE HERE
+  s = "Hello, " + name
+  return s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /^[a-z]/i and s !~ /^[aeiou]/i
+     return true
+  else
+     return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    return false if s.empty?
+    if s !~ /[\D]/ and s !~ /[23456789]/ and s.to_i(2) % 4 == 0
+      return true
+    else
+      return false
+    end
 end
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+     attr_accessor :isbn, :price
+     def initialize(isbn, price)
+         raise ArgumentError if isbn.empty? || price <= 0
+         @isbn = isbn
+         @price = price
+     end
+     
+     def price_as_string
+        "$%0.2f" % @price
+     end
 end
